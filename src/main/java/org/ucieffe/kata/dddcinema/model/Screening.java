f36feeb094b02dd515e1597c54b1dc5385d90425
Davide Di Somma <davide.disomma@gmail.com>
2021-11-10 23:51:55 +0100

    Add UUID to Screening because is an aggregate.

--- src/main/java/org/ucieffe/kata/dddcinema/model/Screening.java
+++ src/main/java/org/ucieffe/kata/dddcinema/model/Screening.java
@@ -2,16 +2,19 @@ package org.ucieffe.kata.dddcinema.model;
 
 
 import java.util.List;
+import java.util.UUID;
 
 public class Screening {
 
+    public final UUID uuid;
     public final Movie movie;
     public final DateTime schedulingTime;
     public final Room room;
     public final List<Seat> availableSeats;
     public final List<Reservation> reservations;
 
-    public Screening(Movie movie, DateTime schedulingTime, Room room, List<Seat> availableSeats, List<Reservation> reservations) {
+    public Screening(UUID uuid, Movie movie, DateTime schedulingTime, Room room, List<Seat> availableSeats, List<Reservation> reservations) {
+        this.uuid = uuid;
         this.movie = movie;
         this.schedulingTime = schedulingTime;
         this.room = room;
